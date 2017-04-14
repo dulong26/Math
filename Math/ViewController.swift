3a5fda7df4ebdee2df2681d06163300db59d0c0e
Tung <tung@techmasters-iMac-7.local>
2017-04-14 16:01:18 +0700

    Random position and sign

--- Math/ViewController.swift
+++ Math/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var lbNum1: UILabel!
     @IBOutlet weak var lbNum2: UILabel!
+    @IBOutlet weak var lbSign: UILabel!
     
     @IBOutlet weak var btResult1: UIButton!
     @IBOutlet weak var btResult2: UIButton!
@@ -32,7 +33,7 @@ class ViewController: UIViewController {
     }
     
     
-        
+    
     
     //Choose random numbers
     func setRandom() {
@@ -40,8 +41,13 @@ class ViewController: UIViewController {
         let random2 = Int(arc4random_uniform(5)) + 1
         lbNum1.text = String(random1)
         lbNum2.text = String(random2)
-        print(random1)
-        print(random2)
+        
+        let randomSign = Int(arc4random_uniform(2))
+        if randomSign == 0 {
+            lbSign.text = "-"
+        } else {
+            lbSign.text = "+"
+        }
         
         setResult(num1: random1, num2: random2)
         
@@ -49,9 +55,19 @@ class ViewController: UIViewController {
     
     //Set results on buttons
     func setResult(num1: Int, num2: Int) {
-        btResult1.setTitle(String(num1), for: .normal)
-        btResult2.setTitle(String(num2), for: .normal)
-        btResult3.setTitle(String(sum(p1: num1, p2: num2)), for: .normal)
+        
+        var rand1, rand2, rand3: Int
+        rand1 = Int(arc4random_uniform(3))
+        repeat {
+            rand2 = Int(arc4random_uniform(3))
+            rand3 = Int(arc4random_uniform(3))
+        } while (rand2 == rand1)||(rand3 == rand2)||(rand1 == rand3)
+        
+        var result = [String(num1), String(num2), String(sum(p1: num1, p2: num2))]
+        
+        btResult1.setTitle(result[rand1], for: .normal)
+        btResult2.setTitle(result[rand2], for: .normal)
+        btResult3.setTitle(result[rand3], for: .normal)
     }
     
     //Calculation sum
