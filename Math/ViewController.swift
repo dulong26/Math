80149985abf12e3b97865cd253b8011ae8d94a48
Nguyen Quynh Chi <nguyenquynhchi@Nguyens-MacBook-Pro.local>
2017-04-17 11:06:20 +0700

    Insert Right & Wrong count

--- Math/ViewController.swift
+++ Math/ViewController.swift
@@ -9,9 +9,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-//    @IBOutlet weak var lbNum1: UILabel!
-//    @IBOutlet weak var lbNum2: UILabel!
     
     @IBOutlet weak var lbNum1: UILabel!
     @IBOutlet weak var lbNum2: UILabel!
@@ -21,14 +18,47 @@ class ViewController: UIViewController {
     @IBOutlet weak var btResult2: UIButton!
     @IBOutlet weak var btResult3: UIButton!
     
+    @IBOutlet weak var lbCountRight: UILabel!
+    @IBOutlet weak var lbCountWrong: UILabel!
+    var countRight = 0
+    var countWrong = 0
+    
+    @IBOutlet weak var lbCountDown: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setRandom()
+        countRight = 0
+        countWrong = 0
+        lbCountRight.text = String(countRight)
+        lbCountWrong.text = String(countWrong)
+        
     }
     
     @IBAction func btAction(_ sender: UIButton) {
+        let choice = Int((sender.titleLabel?.text)!)
+        let num1 = Int(lbNum1.text!)
+        let num2 = Int(lbNum2.text!)
+        let sign = lbSign.text
+        
+        if (sign == "+") {
+            if (num1! + num2!) == choice! {
+                countRight += 1
+            } else {
+                countWrong += 1
+            }
+        } else if sign == "-" {
+            if (num1! - num2!) == choice! {
+                countRight += 1
+            } else {
+                countWrong += 1
+            }
+        }
+        
+        lbCountRight.text = String(countRight)
+        lbCountWrong.text = String(countWrong)
+        
         setRandom()
     }
     
@@ -49,30 +79,41 @@ class ViewController: UIViewController {
             lbSign.text = "+"
         }
         
-        setResult(num1: random1, num2: random2)
+        setResult(num1: random1, num2: random2, sign: lbSign.text!)
         
     }
     
     //Set results on buttons
-    func setResult(num1: Int, num2: Int) {
+    func setResult(num1: Int, num2: Int, sign: String) {
         
+        var rightAnswer = 0
+        if sign == "+" {
+            rightAnswer = plus(p1: num1, p2: num2)
+        } else if sign == "-" {
+            rightAnswer = minus(p1: num1, p2: num2)
+        }
+        
+        var result = [String(num1), String(num2), String(rightAnswer)]
+        
+        //Choose random position for answer
         var rand1, rand2, rand3: Int
         rand1 = Int(arc4random_uniform(3))
         repeat {
             rand2 = Int(arc4random_uniform(3))
             rand3 = Int(arc4random_uniform(3))
         } while (rand2 == rand1)||(rand3 == rand2)||(rand1 == rand3)
-        
-        var result = [String(num1), String(num2), String(sum(p1: num1, p2: num2))]
-        
+     
         btResult1.setTitle(result[rand1], for: .normal)
         btResult2.setTitle(result[rand2], for: .normal)
         btResult3.setTitle(result[rand3], for: .normal)
     }
     
-    //Calculation sum
-    func sum(p1: Int, p2: Int) -> Int {
+    //Calculation
+    func plus(p1: Int, p2: Int) -> Int {
         return (p1 + p2)
+    }
+    func minus(p1: Int, p2: Int) -> Int {
+        return (p1 - p2)
     }
 
 }
